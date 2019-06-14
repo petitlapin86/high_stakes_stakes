35de9936cd76d201fceca830a0634ff387d22c46
petitlapin86 <skinnerpa86@yahoo.com>
2019-06-14 11:15:37 -0400

    spec out game

--- high_stakes_stakes.py
+++ high_stakes_stakes.py
@@ -23,3 +23,29 @@ get permission from the favilitator.
 when submitting your project the instructions for installing the module must be included
 in the instructions for running the code
 """
+
+#spec out ideas for project
+
+# a collection of horses
+# maybe index number equals == horse number
+#maybe make key/value pairs with odds of horse to win
+
+#input and output
+#-----------------------------------------------------------
+#tell user the name of race and horses running
+#display wallet (start with $100)
+#offer more information on horse odds y/n
+# if yes display horse odds
+# ask user which type of bet they would like to make (1, 1,2 or 1,2,3)
+# ask user which horses they want to bet on
+# bet placing - ask user how much they would like to bet
+#confirm total bet (type_of_bet, horses_to_win, bet_amount)
+#if yes, run program
+#display race results
+#display wallet with winnings or losings calculated
+
+#Algorithms
+#--------------------------------------------------------------
+# program that calculates wallet with winnings or losings and creates new total
+# program that randomly selects winners of horses for three different types of bets
+# program that compares winners wirh users bet horses_to_win
