733bc1e6b91c95433d8307557c9b9ba37e747302
petitlapin86 <skinnerpa86@yahoo.com>
2019-06-14 11:04:37 -0400

    docstring

--- high_stakes_stakes.py
+++ high_stakes_stakes.py
@@ -1,1 +1,25 @@
-"""horse race cli game in python"""
+"""horse race cli game in python
+
+REQUIREMENTS:
+at the minimum at least of of each of the following:
+container type (list, tuple, set or dictionary)
+iteration type (for, while)
+condiditional (if)
+try blocks
+user-defined functions
+input and or output file (submit input data)
+user-defined class. the class must be imported by your main program and have the following required structures.
+- at least 1 private and 2 public self attributes
+at least 1 private and 1 public method that takes arguments, return values and are used by your program
+an init() method that takes at least one argument
+a repr() method
+
+provide unit tests that prove your class methods work as expected. the tests should evaluate results using
+the assert statements.
+
+if you desire to include a module that is not part of the standard python build you must first
+get permission from the favilitator.
+
+when submitting your project the instructions for installing the module must be included
+in the instructions for running the code
+"""
