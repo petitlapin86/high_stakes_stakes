2e14ad084e4228da367946596a444b997495dbe1
petitlapin86 <skinnerpa86@yahoo.com>
2019-06-26 13:48:02 -0400

    update progress in docstring

--- high_stakes_stakes.py
+++ high_stakes_stakes.py
@@ -2,19 +2,19 @@
 
 REQUIREMENTS:
 at the minimum at least of of each of the following:
-container type (list, tuple, set or dictionary)
-iteration type (for, while)
-condiditional (if)
-try blocks
-user-defined functions
-input and or output file (submit input data)
-user-defined class. the class must be imported by your main program and have the following required structures.
-- at least 1 private and 2 public self attributes
-at least 1 private and 1 public method that takes arguments, return values and are used by your program
-an init() method that takes at least one argument
-a repr() method
+[ ]container type (list, tuple, set or dictionary)
+[X]iteration type (for, while)
+[X]condiditional (if)
+[ ]try blocks
+[ ]user-defined functions
+[X]input and or output file (submit input data)
+[ ]user-defined class. the class must be imported by your main program and have the following required structures.
+[ ]- at least 1 private and 2 public self attributes
+[ ]at least 1 private and 1 public method that takes arguments, return values and are used by your program
+[ ]an init() method that takes at least one argument
+[ ]a repr() method
 
-provide unit tests that prove your class methods work as expected. the tests should evaluate results using
+[ ]provide unit tests that prove your class methods work as expected. the tests should evaluate results using
 the assert statements.
 
 if you desire to include a module that is not part of the standard python build you must first
@@ -37,7 +37,7 @@ Horses.print_horse_info()
 
 #WALLET
 #------------------------------------------------------------
-wallet = 100  #display wallet (start with $100)
+
 
 
 #offer more information on horse odds y/n
--- wallet.py
+++ wallet.py
@@ -1,3 +1,5 @@
 #wallet
 class Wallet:
+    def __init__():  #this might be a good place to fill this requirement?
     return "This will be the wallet"
+    wallet = 100  #display wallet (start with $100)
