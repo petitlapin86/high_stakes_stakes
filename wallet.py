9479ea267f99653c5bf19cc78396dae08ef7a9c6
petitlapin86 <skinnerpa86@yahoo.com>
2019-06-30 08:10:40 -0400

    minor refactor to wallet class

--- wallet.py
+++ wallet.py
@@ -1,37 +1,35 @@
 
-#wallet
+#Wallet Class keeps provides money to bet with and tracks winnings/losses 
 class Wallet:
-    def __init__(self,wallet):  #this might be a good place to fill this requirement?yes
+    def __init__(self,wallet):  #__init__ method with private __wallet
             self.__wallet = wallet
 
+    def __check_money_in_wallet(self):
+        try:
+            if self.__wallet <= 0:  #If user runs out of money raise an error
+                raise ValueError('Wallet value is less than or equal to zero')
+        except ValueError as e:
+            print("You are out of money. ")
+
     def get_wallet(self):
-        self.__wallet = wallet
+        return self.__wallet
 
     def set_wallet(self,wallet):
         self.__wallet = wallet
-    #wallet = 100  #display wallet (start with $100)
+
     def win(self, amount):
         self.__wallet += amount
 
     def lose(self, amount):
         self.__wallet -= amount
-    #To do : raise exception when wallet value is less than equal to 0
-
-    def __check_money_in_wallet(self):
-        try:
-            #what do i do in here?
-            if self.__wallet <= 0:
-                raise ValueError('Wallet value is less than or equal to zero')
-        except ValueError as e:
-            print("You are out of money. ")#+repr(e) , include this if you want to print line 31
+        self.__check_money_in_wallet()  #check amount in wallet
 
     def __repr__(self):
-        #you can create your custom string
-        wallet_string = "Your current wallet amount is %d"%self.__wallet
+        wallet_string = "Your current wallet amount is %d"%self.__wallet  #display wallet amount
         return wallet_string
 
 #test code
-my_wallet = Wallet(100)
+my_wallet = Wallet(100)  #wallet = 100  #display wallet (start with $100)
 #print(my_wallet)
 #my_wallet.win(50)
 #print(my_wallet)
